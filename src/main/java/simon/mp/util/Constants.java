ed0c46d72503cea047a287bc1d8faac44f79a0d9
simon <simon200093@163.com>
2021-11-30 18:55:56 +0800

    Update

--- src/main/java/simon/mp/util/Constants.java
+++ src/main/java/simon/mp/util/Constants.java
@@ -4,8 +4,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 public class Constants {
-    public static final String Page_Size = "2";
-    public static final int Page_Size_Int = 2;
+    public static final String Page_Size = "10";
+    public static final int Page_Size_Int = 10;
 
     public static final Pageable Default_Page = PageRequest.of(0, Page_Size_Int);
 }
